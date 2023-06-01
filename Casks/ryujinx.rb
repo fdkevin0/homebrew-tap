cfd1fd43c9964cd88ef5406e470e14df072606db
FDKevin <fdkevinzero@gmail.com>
2023-06-01 10:46:19 +0800

    first commit

---
+++ Casks/ryujinx.rb
@@ -0,0 +1,28 @@
+cask "ryujinx" do
+  version "1.1.819"
+  sha256 "890e0e688cb25a15cd0c2a534c97f1572629f5309c0c411c4372d7f808df22d0"
+
+  url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/test-ava-ryujinx-#{version}-macos_universal.app.tar.gz",
+      verified: "github.com/Ryujinx/release-channel-master/"
+  name "Ryujinx"
+  desc "Experimental Nintendo Switch Emulator written in C#"
+  homepage "https://www.ryujinx.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  depends_on macos: ">= :big_sur"
+
+  app "Ryujinx.app"
+
+  uninstall quit: "org.ryujinx.Ryujinx"
+
+  zap trash: [
+    "~/.config/Ryujinx",
+    "~/Library/Saved Application State/org.ryujinx.Ryujinx.savedState",
+  ]
+end
