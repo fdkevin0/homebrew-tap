a053922a9f5e1d559cdd159fb35ad3c941f98080
FDKevin <fdkevinzero@gmail.com>
2023-08-14 13:26:56 +0800

    feat: new cask ryusak

---
+++ Casks/ryusak.rb
@@ -0,0 +1,25 @@
+cask "ryusak" do
+  version "1.6.2"
+  sha256 "fc3d463f51381c4ec051849b5a539de08bcecdcafd9605062d8fb1e19744d709"
+
+  url "https://github.com/Ecks1337/RyuSAK/releases/download/v#{version}/RyuSAK-darwin-arm64-#{version}.zip",
+      verified: "github.com/Ecks1337/RyuSAK/"
+  name "RyuSAK"
+  desc "Experimental Nintendo Switch Emulator written in C#"
+  homepage "https://www.ryujinx.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "RyuSAK.app"
+
+  uninstall quit: "RyuSAK"
+
+  zap trash: [
+    "~/Library/Application Support/RyuSAK",
+  ]
+end
