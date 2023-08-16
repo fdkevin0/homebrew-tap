d9bc31107474bcfd6d8caf03679f3224cd5ddc32
FDKevin <fdkevinzero@gmail.com>
2023-08-17 01:05:13 +0800

    remove verified for RyuSAK

--- Casks/ryusak.rb
+++ Casks/ryusak.rb
@@ -2,8 +2,7 @@ cask "ryusak" do
   version "1.6.2"
   sha256 "fc3d463f51381c4ec051849b5a539de08bcecdcafd9605062d8fb1e19744d709"
 
-  url "https://github.com/Ecks1337/RyuSAK/releases/download/v#{version}/RyuSAK-darwin-arm64-#{version}.zip",
-      verified: "github.com/Ecks1337/RyuSAK/"
+  url "https://github.com/Ecks1337/RyuSAK/releases/download/v#{version}/RyuSAK-darwin-arm64-#{version}.zip"
   name "RyuSAK"
   homepage "https://github.com/Ecks1337/RyuSAK/"
 
